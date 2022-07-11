679914c53576a384d31b49772938bcde5acc7fb7
andresdosantos <andres.dosantosbritoamaral@gmail.com>
2022-07-11 00:34:00 -0300

    feat(global): add fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 
 import { Dashboard } from './src/screens/Dashboard';
 import theme from './src/global/styles/theme';
+import { Text, View } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View>
+        <Text>Carregando</Text>
+      </View>
+    ); // COLOCAR O PREVENT.ASYNC
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
